a2df9ff5a36487e6300ec531f5fadb23822794f0
auryviainfotech <auryvia.infotech@gmailcom>
2025-12-07 00:26:35 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
+  // trailingSlash: true,
   images: {
     unoptimized: true
   },
   assetPrefix: undefined,
   basePath: '',
-  distDir: 'out',
-  allowedDevOrigins: [
-    'http://192.168.0.145:3003'
-  ]
+  // allowedDevOrigins: [
+  //   'http://192.168.0.145:3003'
+  // ]
 }
 
 module.exports = nextConfig
